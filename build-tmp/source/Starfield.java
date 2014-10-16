b2f71becd6b1b866913ddb7fd0f5cbfea9bec6ce
temucher <tjsmuch@gmail.com>
2014-10-16 11:35:52 -0700

    My Starfield program

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,9 +1,23 @@
-//Teagan Mucher, Block 4 AP CS, Lowell High School
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
 
 Particle[] bob = new Particle[400];
 double oddX, oddY;
 boolean seizure, seizureWarning;
-void setup()
+public void setup()
 {
 	background(0);
 	seizureWarning=true;
@@ -15,7 +29,7 @@ void setup()
 	}
 	bob[bob.length-1] = new OddballParticle();
 }
-void draw()
+public void draw()
 {
 	if(frameCount<120)
 	{
@@ -55,19 +69,19 @@ class NormalParticle implements Particle
 		speed=(Math.random()*7+1);
 		angle=(Math.random()*2*(Math.PI));
 	}
-	void move()
+	public void move()
 	{
 		x=x+Math.cos(angle)*speed;
 		y=y+Math.sin(angle)*speed;
 
 	}
-	void show()
+	public void show()
 	{
 		noStroke();
 		fill(r,g,b);
 		ellipse((float)x,(float)y,10,10);
 	}
-	void reset()
+	public void reset()
 	{
 		if(x<0||x>500)
 		{
@@ -101,19 +115,19 @@ class OddballParticle implements Particle
 		speed=(Math.random()*7+1);
 		angle=(Math.random()*2*(Math.PI));
 	}
-	void move()
+	public void move()
 	{
 		angle=(Math.random()*2*(Math.PI));
 		oddX=oddX+Math.cos(angle)*speed;
 		oddY=oddY+Math.sin(angle)*speed;
 	}
-	void show()
+	public void show()
 	{
 		noStroke();
 		fill(r,g,b);
 		ellipse((float)oddX,(float)oddY,20,20);
 	}
-	void reset()
+	public void reset()
 	{
 		if(oddX<0||oddX>500)
 		{
@@ -127,7 +141,7 @@ class OddballParticle implements Particle
 		}
 	}
 }
-void mousePressed()
+public void mousePressed()
 {
 	if(mouseButton == LEFT)
 	{
@@ -142,3 +156,12 @@ void mousePressed()
 
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
